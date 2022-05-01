cddc192f76fc3438955563cdf9827032ecd5ef0c
pream912 <37475546+pream912@users.noreply.github.com>
2022-05-01 14:15:05 +0530

    Already publishing problem fix

--- src/node_rtmp_session.js
+++ src/node_rtmp_session.js
@@ -1067,10 +1067,12 @@ class NodeRtmpSession {
     }
 
     if (context.publishers.has(this.publishStreamPath)) {
-      this.reject();
       Logger.log(`[rtmp publish] Already has a stream. id=${this.id} streamPath=${this.publishStreamPath} streamId=${this.publishStreamId}`);
-      this.sendStatusMessage(this.publishStreamId, 'error', 'NetStream.Publish.BadName', 'Stream already publishing');
-    } else if (this.isPublishing) {
+      Logger.log(`[rtmp deleting] Trying delete stream. id=${this.id} streamPath=${this.publishStreamPath} streamId=${this.publishStreamId}`);
+      context.publishers.delete(this.publishStreamPath);
+      //this.sendStatusMessage(this.publishStreamId, "error", "NetStream.Publish.BadName", "Stream already publishing");
+    }
+    if (this.isPublishing) {
       Logger.log(`[rtmp publish] NetConnection is publishing. id=${this.id} streamPath=${this.publishStreamPath} streamId=${this.publishStreamId}`);
       this.sendStatusMessage(this.publishStreamId, 'error', 'NetStream.Publish.BadConnection', 'Connection already publishing');
     } else {
